5da73ba35e44fd3a42ef96696e274dfe7a204235
Cristina Shapira <dinamic82@gmail.com>
2019-10-31 19:41:02 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/Arrow-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/Arrow-right.svg" alt=""></img>',
+    });
+
+});
